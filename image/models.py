dd9f060310589b11e40c1b62fc3985d9e23b1f8d
martingathu <martin5gathu@gmail.com>
2020-05-22 16:44:59 +0300

    add image path for the image modal class

--- image/models.py
+++ image/models.py
@@ -4,8 +4,30 @@ from django.db import models
 class Category(models.Model):
     category_name = models.CharField(max_length=30)
 
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def get_category(cls):
+        category = cls.objects.all()
+        return category
+
 class Location(models.Model):
     location_name = models.CharField(max_length=30)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def get_locations(cls):
+        locations = cls.objects.all()
+        return locations
 
 
 class Images(models.Model):
@@ -14,4 +36,26 @@ class Images(models.Model):
     description = models.TextField()
     location = models.ForeignKey(Location, on_delete=models.CASCADE,)
     category = models.ForeignKey(Category, on_delete=models.CASCADE,)
-    
+
+    def save_images():
+        self.save()
+
+    def delete_images():
+        self.delete()
+
+    @classmethod
+    def get_images(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def search_by_category(cls, search_word):
+        photos = cls.objects.filter(category__category_name__icontains=search_word)
+
+        return photos
+
+    @classmethod
+    def get_by_location(cls, location):
+        images = Images.objects.filter(location__location_name__icontains=location).all()
+
+        return images
