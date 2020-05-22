630d39b9c461eaffb69afa971fe2815ba6461a91
martingathu <martin5gathu@gmail.com>
2020-05-22 14:39:39 +0300

    add Location modal

--- image/models.py
+++ image/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Category(models.Model):
     category_name = models.CharField(max_length=30)
 
-# class Location(models.Model):
-#     location_name = models.CharField(max_length=30)
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
 
 
 # class Images(models.Model):
