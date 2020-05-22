471dd53e2efb2351ae4a63d48763587c33ad3f42
martingathu <martin5gathu@gmail.com>
2020-05-22 09:05:00 +0300

    initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/martin/Documents/moringa-school-projects/Core/Django/personal-Gallery/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
