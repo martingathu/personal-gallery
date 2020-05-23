ba75292df97fff72496f7ce60a62820fe3620e64
martingathu <martin5gathu@gmail.com>
2020-05-23 19:01:56 +0300

    add url for location

--- image/urls.py
+++ image/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls import url
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, re_path
+from . import views
 
-urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    
+urlpatterns = [
+    re_path('^$', views.index, name='index'),
+    re_path(r'^search/', views.search_results,name='search_results'),
+    re_path(r'^location/(?P<location>\w+)', views.location_images, name='location')
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
