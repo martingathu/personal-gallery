8865fb1e73bba5613e9b858b91e40104f968de67
martingathu <martin5gathu@gmail.com>
2020-05-23 19:02:47 +0300

    add a location view function

--- image/views.py
+++ image/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Images, Location
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+def index(request):
+    images=Images.get_images()
+    locations = Location.get_locations()
+    return render(request, 'index.html', {'images':images, "locations":locations})
+
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        searched_term = request.GET.get("category")
+        searched_pictures = Images.search_by_category(searched_term)
+        message = f"{searched_term}"
+
+        return render(request, 'search.html', {"message": message, "pics": searched_pictures})
+
+    else:
+        message = "You have not searched for any picture"
+        return render(request, 'search.html', {"message": message})
+
+def location_images(request, location):
+    images = Images.get_by_location(location)
+
+    return render(request, 'location.html', {"images":images})
